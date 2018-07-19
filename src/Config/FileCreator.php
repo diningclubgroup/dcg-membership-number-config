3e9b00a7b86b541ead4d6b0587847670b9f20f03
gulzaman.khan <gulzaman.khan@tastecard.co.uk>
2018-07-19 13:41:00 +0100

    initial commit

--- src/Config/FileCreator.php
+++ src/Config/FileCreator.php
@@ -4,28 +4,28 @@ namespace Dcg\Config;
 
 class FileCreator {
 
-	/**
-	 *  Copy package's config file to project
-	 */
-	public static function createConfigFile (\Composer\Script\Event $event)
-	{
-        $sourceFile = self::getSourceDir($event);
-        $destinationFile = self::getSourceDir($event);
+    /**
+     *  Copy package's config file to project
+     */
+    public static function createConfigFile (\Composer\Script\Event $event)
+    {
+        $sourceFile = static::getSourceFile($event);
+        $destinationFile = static::getDestinationFile($event);
 
-		if (!file_exists($destinationFile)) {
-			mkdir(dirname($destinationFile), 0777, true);
-		}
-		if (!file_exists($destinationFile)) {
-			copy($sourceFile, $destinationFile);
-		}
-	}
+        if (!file_exists($destinationFile)) {
+            mkdir(dirname($destinationFile), 0777, true);
+        }
+        if (!file_exists($destinationFile)) {
+            copy($sourceFile, $destinationFile);
+        }
+    }
 
     /**
      * Get the location of the config file to use as an example (template)
      * @param Composer\Script\Event $event
      * @return string
      */
-	protected static function getSourceFile(\Composer\Script\Event $event) {
+    protected static function getSourceFile(\Composer\Script\Event $event) {
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
         return $vendorDir . DIRECTORY_SEPARATOR . 'dcg' . DIRECTORY_SEPARATOR . 'dcg-lib-config' . DIRECTORY_SEPARATOR . 'config.php';
     }
